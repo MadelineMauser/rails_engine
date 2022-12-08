823058c2d0a6e2766e94620c06967e12d390f10d
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-08 10:20:40 -0700

    Add merchant API show action

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -17,4 +17,16 @@ describe "Merchants API" do
       expect(merchant[:name]).to be_a(String)
     end
   end
+  it "sends one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
 end
