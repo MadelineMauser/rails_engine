242259ff481a0a6ffac8c7024a91ff9ecd3bd3c3
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-09 11:03:51 -0700

    Remove unneeded merchant api endpoints

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -33,39 +33,4 @@ describe "Merchants API" do
     expect(merchant).to have_key(:name)
     expect(merchant[:name]).to be_a(String)
   end
-  it "can create a new merchant" do
-    merchant_params = {name: 'John Doe'}
-    headers = {"CONTENT_TYPE" => "application/json"}
-    
-    post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
-    created_merchant = Merchant.last
-    
-    expect(response).to be_successful
-    expect(created_merchant.name).to eq(merchant_params[:name])
-  end
-  it "can update a merchant" do
-    id = create(:merchant).id
-    previous_name = Merchant.last.name
-    merchant_params = {name: "Freddy Silver" }
-    headers = {"CONTENT_TYPE" => "application/json"}
-    
-    patch "/api/v1/merchants/#{id}", headers: headers, params: JSON.generate(merchant: merchant_params)
-
-    merchant = Merchant.find(id)
-
-    expect(response).to be_successful
-    expect(merchant.name).to_not eq(previous_name)
-    expect(merchant.name).to eq(merchant_params[:name])
-  end
-  it "can delete a merchant" do
-    id = create(:merchant).id
-
-    expect(Merchant.count).to eq(1)
-
-    delete "/api/v1/merchants/#{id}"
-   
-    expect(response).to be_successful
-    expect(Merchant.count).to eq(0)
-    expect{Merchant.find(id)}.to raise_error(ActiveRecord::RecordNotFound)
-  end
 end
